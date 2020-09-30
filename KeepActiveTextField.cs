186353ce0dece7da56ba972f1b55cb3e7b626e52
clxrffdman <clxrffdman@gmail.com>
2020-09-30 12:41:02 -0700

    Update KeepActiveTextField.cs

--- KeepActiveTextField.cs
+++ KeepActiveTextField.cs
@@ -5,11 +5,19 @@ using TMPro;
 
 public class KeepActiveTextField : MonoBehaviour
 {
+    /**************************************************************************************************************************************************
+    * Purpose: Forces the text field to be active (can be typed in).
+    * Parameters:
+    *     Arguments: N/A
+    *
+    *     Return: N/A (void function).
+    ***************************************************************************************************************************************************/
     public void KeepActive()
     {
         GetComponent<TMP_InputField>().ActivateInputField();
     }
 
+    //Start runs before the first frame
     void Start()
     {
         KeepActive();
