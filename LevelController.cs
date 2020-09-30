e4cf955d72663f7b17f140cd7c0d7f1723194e5a
clxrffdman <clxrffdman@gmail.com>
2020-09-30 11:03:08 -0700

    Update LevelController.cs
    added comments

--- LevelController.cs
+++ LevelController.cs
@@ -10,9 +10,6 @@ public class LevelController : MonoBehaviour
     public int difficulty;
     public float volume;
 
-    
-
-
     void Awake()
     {
         if (GameObject.Find("Monkey") == null && GameObject.Find("LevelControlLoader") == null)
@@ -21,28 +18,28 @@ public class LevelController : MonoBehaviour
         }
         volume = 0.1f;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /**************************************************************************************************************************************************
+    * Purpose: Loads the first level (tutorial)
+    * Parameters:
+    *     Arguments: N/A
+    *
+    *     Return: N/A (void function).
+    ***************************************************************************************************************************************************/
     public void LoadMap1()
     {
         SceneManager.UnloadSceneAsync("MainMenu");
         SceneManager.LoadScene(2, LoadSceneMode.Additive);
-        GameObject.Find("LineStorage").GetComponent<LineStorage>().currentText = 0;
-        
-        
-        
+        GameObject.Find("LineStorage").GetComponent<LineStorage>().currentText = 0;      
     }
 
+    /**************************************************************************************************************************************************
+    * Purpose: Loads a level based on levelInput integer
+    * Parameters:
+    *     Arguments: int levelInput
+    *
+    *     Return: N/A (void function).
+    ***************************************************************************************************************************************************/
     public void Play(int levelInput)
     {
         print("PLAY");
@@ -50,21 +47,21 @@ public class LevelController : MonoBehaviour
         {
             SceneManager.UnloadSceneAsync("MainMenu");
         }
-
-
-        
         SceneManager.LoadScene(2, LoadSceneMode.Additive);
         level = levelInput;
     }
 
+    /**************************************************************************************************************************************************
+    * Purpose: Coroutine which adds to the level index after a short 0.05sec delay.
+    * Parameters:
+    *     Arguments: N/A
+    *
+    *     Return: N/A (void function).
+    ***************************************************************************************************************************************************/
     public IEnumerator LevelValueAdd()
     {
         yield return new WaitForSecondsRealtime(0.05f);
-        level++;
-
-        
+        level++;       
     }
-
-
 
 }
