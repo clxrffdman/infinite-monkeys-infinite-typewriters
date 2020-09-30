9928a8bf79ecdadd5935f2f72678f8f1b1e6b1d1
clxrffdman <clxrffdman@gmail.com>
2020-09-29 23:02:53 -0700

    added missing file
    This file manipulates the UI sliders in the game.

---
+++ SliderValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+    public int genre;
+    public Slider slider;
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Level " + slider.value + "" ;
+    }
+}
